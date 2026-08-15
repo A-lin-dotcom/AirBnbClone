9251c3827fdcd5450fff78337d164aae8d6f867c
Alin Das <alindas2006@gmail.com>
2026-08-15 19:25:21 +0530

    update database connection to use environment variable for MongoDB URL

--- init/index.js
+++ init/index.js
@@ -5,7 +5,7 @@ const data = require('./data.js');
 main().then(()=>console.log('Connected to MongoDB'))
 .catch(err=>console.log(err));
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect(process.env.ATLASDB_URL);
 }
 
 const initDb=async()=>{
